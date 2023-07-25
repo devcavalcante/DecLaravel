d1a46a74457aeac3a64cd274ff738212488f5f89
Henrique Mota <henriquemota0110@gmail.com>
2023-07-25 10:52:46 -0300

    restaurando o arquivo ExampleTest.php do feature

---
+++ tests/Feature/ExampleTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+
+/**
+ *
+ */
+class ExampleTest extends TestCase
+{
+    /**
+     * A basic test example.
+     * return void
+     */
+    public function testTheApplicationReturnsASuccessfulResponse(): void
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+}
