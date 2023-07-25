dcece63132c910bd3b745856af9f58c1df4c21a3
Henrique Mota <henriquemota0110@gmail.com>
2023-07-25 10:44:28 -0300

    restaurando o arquivo ExampleTest.php do unit

---
+++ tests/Unit/ExampleTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ *
+ */
+class ExampleTest extends TestCase
+{
+    /**
+     * A basic test example.
+     * return void
+     */
+    public function testThatTrueIsTrue(): void
+    {
+        $this->assertTrue(true);
+    }
+}
