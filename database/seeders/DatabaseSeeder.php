fc7f05785cac7c0a40b47b0ff070935038afecf4
debcavalcante <debora.cavalcante@picpay.com>
2023-08-27 16:32:48 -0300

    ajustando seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,6 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call(TypeUserSeeder::class);
-        $this->call([UserSeeder::class]);
+        $this->call(UserSeeder::class);
     }
 }
