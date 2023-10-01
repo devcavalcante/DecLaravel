c5881a0c4a6008727e5c542e042118f95ff1a06e
devcavalcante <deborabc25@gmail.com>
2023-10-01 15:03:41 -0300

    ajustando erro de cors

---
+++ app/Http/Middleware/Cors.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class Cors
+{
+    public function handle($request, Closure $next)
+    {
+        return $next($request)
+            ->header('Access-Control-Allow-Origin', "*")
+            ->header('Access-Control-Allow-Methods', "PUT, POST, DELETE, GET, OPTIONS, PATCH")
+            ->header('Access-Control-Allow-Headers', "Accept, Authorization, Content-Type");
+    }
+}
