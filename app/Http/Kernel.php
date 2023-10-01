28c9d4750249fabceb8092161dd6cd181203d244
devcavalcante <deborabc25@gmail.com>
2023-10-01 15:04:33 -0300

    phpcs

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -44,7 +44,7 @@ class Kernel extends HttpKernel
         ValidatePostSize::class,
         TrimStrings::class,
         ConvertEmptyStringsToNull::class,
-        Cors::class
+        Cors::class,
     ];
 
     /**
